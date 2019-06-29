d86a792ac9c468859ecfe7e137086eda880cdb83
ozoubia <ozoubia@gmail.com>
2019-06-30 01:27:34 +0300

    finished the whole example

--- effect.js
+++ effect.js
@@ -9,13 +9,56 @@ const typewriter = function(textElemenet, words, wait = 3000) {
 };
 
 // type method
-typewriter.prototype.type = function() {};
+typewriter.prototype.type = function() {
+	//current index of word
+	const current = this.wordIndex % this.words.length;
+	// get full text of current word
+	const fulltext = this.words[current];
+	console.log(fulltext);
+
+	//check if deleting
+	if (this.isDeleting) {
+		//remove a character
+		this.txt = fulltext.substring(0, this.txt.length - 1);
+	}
+	else {
+		//add a character
+		this.txt = fulltext.substring(0, this.txt.length + 1);
+	}
+
+	//insert txt into element
+	this.textElemenet.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+	//initial type speed
+	let typespeed = 300;
+
+	if (this.isDeleting) {
+		typespeed /= 2;
+	}
+
+	// if the word is complete
+	if (!this.isDeleting && this.txt === fulltext) {
+		//make a pause at the end
+		typespeed = this.wait;
+		//set delete to true
+		this.isDeleting = true;
+	}
+	else if (this.isDeleting && this.txt === '') {
+		this.isDeleting = false;
+		//move to the next word
+		this.wordIndex++;
+		//pause before starting to type again
+		typespeed = 500;
+	}
+
+	setTimeout(() => this.type(), typespeed);
+};
 
 //Init on DOM loads
 document.addEventListener('DOMContentLoaded', init);
 //init app
 function init() {
-	const textElemenet = document.querySelector('txt-type');
+	const textElemenet = document.querySelector('.text-type');
 	const words = JSON.parse(textElemenet.getAttribute('data-words'));
 	const wait = textElemenet.getAttribute('data-wait');
 	//init typewriter

917ec4cd5dce202df78caa25a7b8c308cef10ff4
ozoubia <ozoubia@gmail.com>
2019-06-30 23:38:24 +0300

    added a cool background move effect when moving the mouse

--- effect.js
+++ effect.js
@@ -64,4 +64,17 @@ function init() {
 	const wait = textElemenet.getAttribute('data-wait');
 	//init typewriter
 	new typewriter(textElemenet, words, wait);
+
+	//moving the background image on mouse move
+
+	var movementStrength = 25;
+	var height = movementStrength / $(window).height();
+	var width = movementStrength / $(window).width();
+	$('body').mousemove(function(e) {
+		var pageX = e.pageX - $(window).width() / 2;
+		var pageY = e.pageY - $(window).height() / 2;
+		var newvalueX = width * pageX * -1 - 25;
+		var newvalueY = height * pageY * -1 - 50;
+		$('body').css('background-position', newvalueX + 'px     ' + newvalueY + 'px');
+	});
 }
